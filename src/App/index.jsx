cea19cfdf65c5f2d56418b4e736ff171edfb282f
Aia Rupsom <aiale88@gmail.com>
2019-09-19 09:52:37 +0200

    create project and basic structure

---
+++ src/App/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div className="App">App</div>;
+  }
+}
+
+export default App;
