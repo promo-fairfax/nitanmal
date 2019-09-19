b7c7e7fba64a43cd5119891ac20fec9436013813
Aia Rupsom <aiale88@gmail.com>
2019-09-19 09:59:38 +0200

    add react router

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import Home from '../Home/index';
 
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {};
   }
 
   render() {
-    return <div className="App">App</div>;
+    return (
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </main>
+      </div>
+    );
   }
 }
 
