c7cf7f2c704d540ea3663337ecbc9bbb34c43448
m-m-e <megmyfedwards@gmail.com>
2019-09-21 16:55:28 +0200

    Add link to share on twitter

--- src/components/RandomCats/index.jsx
+++ src/components/RandomCats/index.jsx
@@ -20,25 +20,36 @@ class RandomCats extends Component {
   render(){
     const { catData } = this.state;
     const { handleCatFetch } = this;
-    return(
+    return (
       <div className="random__cat-container">
         <h2 className="random__cat-title">Welcome to the random cat page</h2>
-        <h3 className="random__cat-subtitle">Click on the button to get a random cat!</h3>
-        <button 
-          className="random__cat-button"
-          onClick={handleCatFetch}
-        >
+        <h3 className="random__cat-subtitle">
+          Click on the button to get a random cat!
+        </h3>
+        <button className="button random__cat-button" onClick={handleCatFetch}>
           Click me
         </button>
-        {catData.url &&
-          <img 
+        {catData.url && (
+          <img
             src={catData.url}
             alt={`Random cat ${catData.id}`}
             className="random__cat-image"
           />
-        }
+        )}
+        {catData.url && (
+          <div className="twitter__container">
+            <a
+              className="button twitter__button"
+              href={`http://twitter.com/share?text=Here is my favourite new cat&url=${catData.url}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Share this cat picture on Twitter!
+            </a>
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
 
