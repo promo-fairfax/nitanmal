a0b8ec690aceb4bb10a93fb3e525ce8057dd6a5c
Carla Rodriguez <n298775@gruposantander.com>
2019-09-29 19:04:07 +0200

    get typing effect running

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
+import ReactTypingEffect from 'react-typing-effect';
 import './Home.scss';
 import BeerBtn from '../BeerBtn';
 import Chatbot from '../Chatbot/index';
 import Tea from '../Tea/index';
 
 class Home extends Component {
+  ReactTypingEffectDemo = () => {
+    return (
+      <ReactTypingEffect
+        text="Hello."
+      />
+    );
+  };
+
+
   render () {
     const { chatbotOpen, handleChatbot, btnBeer, onClickClose, closeModal, onClickBeer, handleTea, tea  } = this.props; 
     return(
       <div className="home">
         <h1 className="home__title">Ni Tan Mal</h1>
+        {this.ReactTypingEffectDemo()}
         <div className={`home__chatbot ${chatbotOpen ? 'open' : 'closed'}`}>
           {chatbotOpen ? 
             <Chatbot handleChatbot={handleChatbot} />
