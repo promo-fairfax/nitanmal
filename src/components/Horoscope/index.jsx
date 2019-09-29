fc5552f19c247ed8869de228f2ca0bd318987eb4
lararb <lararb91@gmail.com>
2019-09-29 19:56:51 +0200

    Add emojis

--- src/components/Horoscope/index.jsx
+++ src/components/Horoscope/index.jsx
@@ -19,8 +19,10 @@ class Horoscope extends React.Component {
 
   getResultHoroscope(event) {
     event.preventDefault();
-    const result = this.data1 + this.data2 + this.data3 + this.data4;
-    this.props.getHoroscope(result);
+    if(this.data1 !== 0 || this.data2 !== 0 || this.data3 !== 0 || this.data4 !== 0) {
+      const result = this.data1 + this.data2 + this.data3 + this.data4;
+      this.props.getHoroscope(result);
+    }
   }
 
 
@@ -41,7 +43,20 @@ class Horoscope extends React.Component {
     this.data4 = parseInt(radioValue);
   }
 
+  showEmoji(emojiAsign) {
+    if(emojiAsign === 'unicornio') {
+      return 'images/emojis/unicorn.png'
+    } else if(emojiAsign === 'pizza') {
+      return 'images/emojis/pizza.png'
+    } else if (emojiAsign === 'caca') {
+      return 'images/emojis/caca.png'
+    }
+  }
+
   render() {
+
+const { horoscope } = this.props;
+
     return(
       <div className="horoscope">
 
@@ -49,16 +64,16 @@ class Horoscope extends React.Component {
           <fieldset className="horoscope__form--fieldset">
             <legend className="horoscope__form--legend">¿Qué día crees que es hoy?</legend>
               <label htmlFor="1" className="horoscope__label">
-                <input onChange={this.sendValueToConstructor1} type="radio" className="horoscope__input" name="1" id="1" value="4"/> 
+                <input onChange={this.sendValueToConstructor1} type="radio" className="horoscope__input" name="1" id="1" value="2"/> 
                 ¿Hoy es viernes? ¡No puede ser!
               </label>
               <label htmlFor="2" className="horoscope__label">
                 <input onChange={this.sendValueToConstructor1} type="radio" className="horoscope__input" name="1" id="2" value="3"/> 
-                Respuesta 2
+                Hoy es martes, seguro.
               </label>
               <label htmlFor="3" className="horoscope__label">
                 <input onChange={this.sendValueToConstructor1} type="radio" className="horoscope__input" name="1" id="3" value="1"/> 
-                Respuesta 3
+                Domingo, no puedo desayunar en el bar de abajo.
               </label>
           </fieldset>
           <fieldset className="horoscope__form--fieldset">
@@ -68,7 +83,7 @@ class Horoscope extends React.Component {
                 Un té rico.
               </label>
               <label htmlFor="5" className="horoscope__label">
-                <input onChange={this.sendValueToConstructor2} type="radio" className="horoscope__input" name="2" id="6" value="4"/>
+                <input onChange={this.sendValueToConstructor2} type="radio" className="horoscope__input" name="2" id="6" value="2"/>
                 Una Alhambra fresquita.
               </label>
               <label htmlFor="6" className="horoscope__label">
@@ -94,16 +109,25 @@ class Horoscope extends React.Component {
           <fieldset className="horoscope__form--fieldset">
             <legend className="horoscope__form--legend"><em>Bonus</em> ¿Estás esperando un pedido de Seur?</legend>  
               <label htmlFor="10" className="horoscope__label">
-                <input onChange={this.sendValueToConstructor4} type="radio" className="horoscope__input" name="4" id="13" value="2"/>
+                <input onChange={this.sendValueToConstructor4} type="radio" className="horoscope__input" name="4" id="13" value="-10"/>
                 Sí
               </label>
               <label htmlFor="11" className="horoscope__label">
-                <input onChange={this.sendValueToConstructor4} type="radio" className="horoscope__input" name="4" id="14" value="4"/>
+                <input onChange={this.sendValueToConstructor4} type="radio" className="horoscope__input" name="4" id="14" value="0"/>
                 No.
               </label>
             </fieldset>
           <button onClick={this.getResultHoroscope} className="form__button" type="submit">click</button>
         </form>
+        { horoscope !== '' ?
+          <div className='horoscope__modal--container'>
+            <div className='horoscope__modal--content'>
+              <img className='horoscope__modal--emoji' src={this.showEmoji(horoscope)} alt='your emoji'></img>
+            </div>
+          </div>
+          :
+          null
+        }
       </div>
     )
   }
