c770a062d60cac8fbce2fe2625a41b8d6cb3edcb
lararb <lararb91@gmail.com>
2019-10-04 00:20:28 +0200

    El build de las narices

---
+++ docs/precache-manifest.4d7b0a8286a52d647942b70c160a8ccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb0b8d1a42bcd0f968af7bab296fab0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "22a130f9265ecfacc9b6",
+    "url": "./static/css/main.547b8342.chunk.css"
+  },
+  {
+    "revision": "194c0d070c795665dd53",
+    "url": "./static/js/2.260165dd.chunk.js"
+  },
+  {
+    "revision": "22a130f9265ecfacc9b6",
+    "url": "./static/js/main.6cafbee1.chunk.js"
+  },
+  {
+    "revision": "8f8a15e0cd831693db35",
+    "url": "./static/js/runtime~main.3359c0ad.js"
+  },
+  {
+    "revision": "917b0c0b39a329ffc4ecc081be7615d9",
+    "url": "./static/media/brain.917b0c0b.rive"
+  }
+]);
